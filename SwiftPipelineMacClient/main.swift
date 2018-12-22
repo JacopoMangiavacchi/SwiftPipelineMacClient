da40cb03eefcb001b82a185dadac6cdd77d28401
Jacopo Mangiavacchi <jamangia@microsoft.com>
2018-12-21 18:32:40 -0800

    Initial commit

---
+++ SwiftPipelineMacClient/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPipelineMacClient
+//
+//  Created by Jacopo Mangiavacchi on 12/21/18.
+//  Copyright © 2018 Jacopo Mangiavacchi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
